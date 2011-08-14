17ce0660c5f029d48f5d1f30f3958f3d59053d14
xiaoxin:wq <593748946@qq.com>
2011-08-14 08:22:55 +0800

    复制 代码

--- a.c
+++ a.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 #define n 20
-char *copy(char *a, char*b);
+char copy(char *a, char*b);
 int main(int argc, const char *argv[])
 {
     char a[n]="ajsajfjgfhg";
     char b[n];
     copy(b,a);
-
-    printf("%s",b);
-
+    printf("%s\n",b);
     return 0;
 }
-char  *copy(char *a, char*b)
+char  copy(char *a, char*b)
 {
    while(*b!='\0')
     {
-
    // char *m;
        *a=*b;
        a++;
        b++;
     }
-
     *a='\0';
-
-    return a;////函数返回值的类型与函数本身的类型相同
-   
+ //   return a;////函数返回值的类型与函数本身的类型相同
 }
